ff31b5402b534e738e2d8bddd654c0d6f6f95bbb
Watson <watson1978@gmail.com>
2018-09-07 18:42:00 +0900

    AlphaChannelType -> AlphaChannelOption

--- ext/RMagick/rmimage.c
+++ ext/RMagick/rmimage.c
@@ -574,7 +574,7 @@ VALUE
 Image_alpha(int argc, VALUE *argv, VALUE self)
 {
     Image *image;
-    AlphaChannelType alpha;
+    AlphaChannelOption alpha;
 
 
     // For backward compatibility, make alpha() act like alpha?
@@ -589,7 +589,7 @@ Image_alpha(int argc, VALUE *argv, VALUE self)
 
 
     image = rm_check_frozen(self);
-    VALUE_TO_ENUM(argv[0], alpha, AlphaChannelType);
+    VALUE_TO_ENUM(argv[0], alpha, AlphaChannelOption);
 
 #if defined(HAVE_SETIMAGEALPHACHANNEL)
     // Added in 6.3.6-9
