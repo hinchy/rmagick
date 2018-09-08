2de4609ade4b9c204f3986f6d9df30748b93e820
Watson <watson1978@gmail.com>
2018-09-08 18:05:49 +0900

    rm_pixelpacket_to_color_name_info -> rm_pixelinfo_to_color_name_info

--- ext/RMagick/rminfo.c
+++ ext/RMagick/rminfo.c
@@ -492,7 +492,7 @@ Info_background_color(VALUE self)
     Info *info;
 
     Data_Get_Struct(self, Info, info);
-    return rm_pixelpacket_to_color_name_info(info, &info->background_color);
+    return rm_pixelinfo_to_color_name_info(info, &info->background_color);
 }
 
 
@@ -538,7 +538,7 @@ Info_border_color(VALUE self)
     Info *info;
 
     Data_Get_Struct(self, Info, info);
-    return rm_pixelpacket_to_color_name_info(info, &info->border_color);
+    return rm_pixelinfo_to_color_name_info(info, &info->border_color);
 }
 
 /**
@@ -1680,7 +1680,7 @@ Info_matte_color(VALUE self)
     Info *info;
 
     Data_Get_Struct(self, Info, info);
-    return rm_pixelpacket_to_color_name_info(info, &info->matte_color);
+    return rm_pixelinfo_to_color_name_info(info, &info->matte_color);
 }
 
 /**
@@ -2285,7 +2285,7 @@ Info_transparent_color(VALUE self)
     Info *info;
 
     Data_Get_Struct(self, Info, info);
-    return rm_pixelpacket_to_color_name_info(info, &info->transparent_color);
+    return rm_pixelinfo_to_color_name_info(info, &info->transparent_color);
 }
 
 
