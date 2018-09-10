265d0dfe3fcfe5495ec852524dee001d5a862c1e
Watson <watson1978@gmail.com>
2018-09-10 14:04:50 +0900

    fix thresholder() argument

--- ext/RMagick/rmimage.c
+++ ext/RMagick/rmimage.c
@@ -13463,6 +13463,7 @@ VALUE threshold_image(int argc, VALUE *argv, VALUE self, thresholder_t threshold
     Image *image, *new_image;
     double red, green, blue, opacity;
     char ctarg[200];
+    ExceptionInfo *exception;
 
     image = rm_check_destroyed(self);
 
@@ -13496,8 +13497,10 @@ VALUE threshold_image(int argc, VALUE *argv, VALUE self, thresholder_t threshold
 
     new_image = rm_clone_image(image);
 
-    (thresholder)(new_image, ctarg);
-    rm_check_image_exception(new_image, DestroyOnError);
+    exception = AcquireExceptionInfo();
+    (thresholder)(new_image, ctarg, exception);
+    rm_check_exception(exception, new_image, DestroyOnError);
+    (void) DestroyExceptionInfo(exception);
 
     return rm_image_new(new_image);
 }
