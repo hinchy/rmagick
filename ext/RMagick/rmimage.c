9ad90d1af764521811d806bb5cd6dcc536150d2e
Watson <watson1978@gmail.com>
2018-09-10 10:08:18 +0900

    fix NegateImage() argument

--- ext/RMagick/rmimage.c
+++ ext/RMagick/rmimage.c
@@ -9214,6 +9214,7 @@ Image_negate(int argc, VALUE *argv, VALUE self)
 {
     Image *image, *new_image;
     unsigned int grayscale = MagickFalse;
+    ExceptionInfo *exception;
 
     image = rm_check_destroyed(self);
     if (argc == 1)
@@ -9227,8 +9228,10 @@ Image_negate(int argc, VALUE *argv, VALUE self)
 
     new_image = rm_clone_image(image);
 
-    (void) NegateImage(new_image, grayscale);
-    rm_check_image_exception(new_image, DestroyOnError);
+    exception = AcquireExceptionInfo();
+    (void) NegateImage(new_image, grayscale, exception);
+    rm_check_exception(exception, new_image, DestroyOnError);
+    (void) DestroyExceptionInfo(exception);
 
     return rm_image_new(new_image);
 }
