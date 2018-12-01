1071ad0c778179aa6c5ac2123126442572bc9880
Watson <watson1978@gmail.com>
2018-12-02 01:19:01 +0900

    fix Image_matte()

--- ext/RMagick/rmimage.c
+++ ext/RMagick/rmimage.c
@@ -8611,7 +8611,7 @@ Image_matte(VALUE self)
     Image *image;
 
     image = rm_check_destroyed(self);
-    return image->matte ? Qtrue : Qfalse;
+    return (image->alpha_trait != UndefinedPixelTrait) ? Qtrue : Qfalse;
 }
 
 
